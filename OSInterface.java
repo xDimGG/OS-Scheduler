061119ed2f91ac9f41642b929c77c49da99f4a23
Angelo Lloti <angelolloti@Angelos-MacBook-Air.local>
2022-10-11 14:44:11 -0400

    add new project 4 files

--- OSInterface.java
+++ OSInterface.java
@@ -1,9 +1,2 @@
-public interface OSInterface {
-	int CreateProcess(UserlandProcess myNewProcess, PriorityEnum priority);
-
-	boolean DeleteProcess(int processId);
-
-	void Sleep(int milliseconds);
-	void run();
+public interface OSInterface extends ProcessInterface, Device {
 }
-
